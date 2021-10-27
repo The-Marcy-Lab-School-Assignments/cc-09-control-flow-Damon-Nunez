0d2a36123698f020aa7eb8638cf7c02570cfc2a0
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-10-27 14:18:03 +0000

    Finished

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(num){
+  if(num >= 5 && num <= 10){
+      console.log("true");
+  }else 
+  console.log("false");
 }
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(num){
+    if(num >= 10 && num <= 30){
+      console.log("true");
+  }else 
+  console.log("false");
 }
 
 //3
-function betweenThirtyAndFifty(){
+function betweenThirtyAndFifty(num){
   
+      if(num >= 30 && num <= 50){
+      console.log("true");
+  }else 
+  console.log("false");
 }
-
-
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(num){
+  if(num <= 30 && num >= 0) {
+      console.log("true");
+  }else if(num >= 50 && num <= 70){
+      console.log("true");
+  }else {
+      console.log("false");
+  }
 }
 
 //5
-function betweenTwentyFiftySixty(){
+function betweenTwentyFiftySixty(num){
   
+  
+    if(num <= 20 && num >= 0) {
+      console.log("true");
+  }else if(num >= 50 && num <= 60){
+      console.log("true");
+  }else {
+      console.log("false");
+  }
 }
 
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(num){
+ 
+   if(num <= 10 && num >= 0) {
+      console.log("true");
+  }else if(num >= 20 && num <= 30){
+      console.log("true");
+  }else {
+      console.log("false");
+  }
 }
+betweenFiveAndTen(8)
+betweenTenAndThirty(40)
+betweenThirtyAndFifty(80)
+betweenThirtyFiftySeventy(100)
+betweenTwentyFiftySixty(10000)
+betweenTenTwentyThirty(40)
